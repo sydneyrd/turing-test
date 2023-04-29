4d00c4c08c92710ff71c2df4347b5b1b0814e1ad
Sydney Dickson <sydneyrachelledickson@gmail.com>
2023-04-29 14:01:13 -0500

    working with 1 ai instance

--- server/my_http/openai.js
+++ server/my_http/openai.js
@@ -3,15 +3,13 @@ const { systemMessage } = require("./managers");
 
 
 async function fetchGPTData  (name, messages){
-
-
     const key = process.env.OPENAI_KEY;
     try {
         // i need to take the messages array and change the key 'message' to 'content' before sending the new array
         const message = messages.map(message => {
             return {content: message.message, role: 'user'}
         })
-        new_array = systemMessage(name).concat(messages) //double check this
+        new_array = systemMessage(name).concat(message) //double check this
       const response = await fetch('https://api.openai.com/v1/chat/completions', {
         method: 'POST',
         headers: {
@@ -20,7 +18,7 @@ async function fetchGPTData  (name, messages){
         },
         body: JSON.stringify({
             model: "gpt-3.5-turbo",
-            messages: message,
+            messages: new_array,
             temperature: 1.2,
             top_p: 1,
         }),
